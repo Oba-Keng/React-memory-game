c2cc3bb26b75fffcaf46167d76f5aac34ecafbf2
Obakeng <obeezus@gmail.com>
2020-04-25 01:33:27 +0200

    board updates

--- frontend/src/components/regions/Game/presentation.js
+++ frontend/src/components/regions/Game/presentation.js
@@ -4,16 +4,16 @@ import Board from "../Cards/index";
 const Game = props => {
   const [game, setGame] = useState({
     cardclick: 0,
-    history: [{ cards: Array(30).fill(null) }]
+    phase: [{ cards: Array(30).fill(null) }]
   });
 
-  const history = game.history;
+  const phase = game.phase;
 
   return (
     <React.Fragment>
       <div className="game">
         <div className="">
-          <Board onClick={i => props.onClick(i)} cards={history} />
+          <Board onClick={i => props.onClick(i)} cards={phase} />
         </div>
       </div>
     </React.Fragment>
